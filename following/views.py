e3d4add3a8464bd021ae87c2f301d82cce47c4d7
microStationCorp <sujanmondal916@gmail.com>
2020-02-13 00:21:01 +0530

    timeline as per follow

--- following/views.py
+++ following/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from timeline.models import Follower
 
 # Create your views here.
+
+
 def following(response):
-    pass
+    if response.method == "GET" and response.is_ajax():
+        followingId = response.GET['id']
+        if response.GET['indicator'] == 'Follow':
+            Follower.objects.Create(
+                follower_id=response.user.id, following_id=followingId)
+        elif response.GET['indicator'] == 'Following':
+            Follower.objects.filter(follower_id=response.user.id,
+                                    following_id=followingId)[0].delete()
+        return HttpResponse('')
+    else:
+        return HttpResponse('''<h1 style="border-bottom: 1px solid #aaa; padding: 10px" > Not Found(  # 404)</h1>
+
+    <div class="alert alert-danger" >
+        Page not found. </div >
+
+    <p>
+        The above error occurred while the Web server was processing your request.
+    </p>
+    <p>
+        Please contact us if you think this is a server error. Thank you.
+    </p>''')
--- timeline/views.py
+++ timeline/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from userposts.models import Posts
+from timeline.models import Follower
 # Create your views here.
 
 
 @login_required(login_url='../login')
 def timeline(response):
-    allPosts = Posts.objects.all().order_by('-dateOfPost')
+    myFollowing = Follower.objects.filter(follower_id=response.user.id)
+    ids = []
+    for f in myFollowing:
+        ids.append(f.id)
+    allPosts = Posts.objects.filter(author_id__in=ids).order_by('-dateOfPost')
     return render(response, 'timeline/timeline.html', {'posts': allPosts})

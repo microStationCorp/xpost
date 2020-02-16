a8b926fc9c02b628911f405eceee79348daf0cc0
microStationCorp <sujanmondal916@gmail.com>
2020-02-16 21:44:35 +0530

    bug fixes

--- userposts/views.py
+++ userposts/views.py
@@ -14,5 +14,18 @@ def userPost(response):
 
 @login_required(login_url='../login')
 def deletePost(response, id):
-    Posts.objects.filter(id=id)[0].delete()
-    return redirect('../posts')
+    if len(Posts.objects.filter(id=id, author_id=response.user.id)) != 0:
+        Posts.objects.filter(id=id)[0].delete()
+        return redirect('../posts')
+    else:
+        return HttpResponse('''<h1 style="border-bottom: 1px solid #aaa; padding: 10px" > Not Found(  # 404)</h1>
+
+    <div class="alert alert-danger" >
+        Page not found. </div >
+
+    <p>
+        The above error occurred while the Web server was processing your request.
+    </p>
+    <p>
+        Please contact us if you think this is a server error. Thank you.
+    </p>''')

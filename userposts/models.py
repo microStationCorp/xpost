da8bc6affe32bf8a27e6db345d8b1c10bba3d018
microStationCorp <sujanmondal916@gmail.com>
2020-02-23 23:55:27 +0530

    like report dislike count added in database and diplayed in mypost

--- userposts/models.py
+++ userposts/models.py
@@ -5,10 +5,16 @@ from django.utils import timezone
 
 
 class Posts(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_author')
     title = models.CharField(max_length=150)
     post = models.TextField()
     dateOfPost = models.DateTimeField(default=timezone.now)
+    like = models.ManyToManyField(User, related_name="liked_user", blank=True)
+    dislike = models.ManyToManyField(
+        User, related_name="disliked_user", blank=True)
+    report = models.ManyToManyField(
+        User, related_name="reported_user", blank=True)
 
     def __str__(self):
         return self.title
